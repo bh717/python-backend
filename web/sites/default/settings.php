3936a92bf457fa26241696fe6dcdd971432570e7
WebDev717 <lasse.lin717@gmail.com>
2017-07-23 17:12:52 +0530

    Add trusted host settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -734,6 +734,10 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * example.org, with all subdomains included.
  */
 
+$settings['trusted_host_patterns'] = [
+  '^(www\.)?contrib.axelerant.(com|dev)$',
+];
+
 /**
  * The default list of directories that will be ignored by Drupal's file API.
  *
