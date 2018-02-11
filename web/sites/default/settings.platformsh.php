52eef527fb1d139e4bd48988d8c89c965a06cc3a
WebDev717 <lasse.lin717@gmail.com>
2018-02-11 22:55:07 +0530

    Add Redis and update everything

--- web/sites/default/settings.platformsh.php
+++ web/sites/default/settings.platformsh.php
@@ -5,42 +5,94 @@
  */
 
 // Configure the database.
-if (isset($_ENV['PLATFORM_RELATIONSHIPS'])) {
-  $relationships = json_decode(base64_decode($_ENV['PLATFORM_RELATIONSHIPS']), TRUE);
+if (getenv('PLATFORM_RELATIONSHIPS')) {
+  $relationships = json_decode(base64_decode(getenv('PLATFORM_RELATIONSHIPS')), TRUE);
 
-  if (empty($databases['default']['default']) && !empty($relationships['database'])) {
-    foreach ($relationships['database'] as $endpoint) {
-      $database = [
-        'driver' => $endpoint['scheme'],
-        'database' => $endpoint['path'],
-        'username' => $endpoint['username'],
-        'password' => $endpoint['password'],
-        'host' => $endpoint['host'],
-        'port' => $endpoint['port'],
-      ];
+  if (empty($databases['default']) && !empty($relationships)) {
+    foreach ($relationships as $key => $relationship) {
+      $drupal_key = ($key === 'database') ? 'default' : $key;
+      foreach ($relationship as $instance) {
+        if (empty($instance['scheme']) || ($instance['scheme'] !== 'mysql' && $instance['scheme'] !== 'pgsql')) {
+          continue;
+        }
+        $database = [
+          'driver' => $instance['scheme'],
+          'database' => $instance['path'],
+          'username' => $instance['username'],
+          'password' => $instance['password'],
+          'host' => $instance['host'],
+          'port' => $instance['port'],
+        ];
 
-      if (!empty($endpoint['query']['compression'])) {
-        $database['pdo'][PDO::MYSQL_ATTR_COMPRESS] = TRUE;
-      }
+        if (!empty($instance['query']['compression'])) {
+          $database['pdo'][PDO::MYSQL_ATTR_COMPRESS] = TRUE;
+        }
 
-      if (!empty($endpoint['query']['is_master'])) {
-        $databases['default']['default'] = $database;
-      }
-      else {
-        $databases['default']['replica'][] = $database;
+        if (!empty($instance['query']['is_master'])) {
+          $databases[$drupal_key]['default'] = $database;
+        }
+        else {
+          $databases[$drupal_key]['replica'][] = $database;
+        }
       }
     }
   }
+
+  if (!empty($relationships['redis'][0]) && !drupal_installation_attempted() && extension_loaded('redis')) {
+    $redis = $relationships['redis'][0];
+
+    // Set Redis as the default backend for any cache bin not otherwise specified.
+    $settings['cache']['default'] = 'cache.backend.redis';
+    $settings['redis.connection']['host'] = $redis['host'];
+    $settings['redis.connection']['port'] = $redis['port'];
+
+    // Allow the services to work before the Redis module itself is enabled.
+    $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
+
+    // Manually add the classloader path, this is required for the container cache bin definition below
+    // and allows to use it without the redis module being enabled.
+    $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
+
+    // Use redis for container cache.
+    // The container cache is used to load the container definition itself, and
+    // thus any configuration stored in the container itself is not available
+    // yet. These lines force the container cache to use Redis rather than the
+    // default SQL cache.
+    $settings['bootstrap_container_definition'] = [
+      'parameters' => [],
+      'services' => [
+        'redis.factory' => [
+          'class' => 'Drupal\redis\ClientFactory',
+        ],
+        'cache.backend.redis' => [
+          'class' => 'Drupal\redis\Cache\CacheBackendFactory',
+          'arguments' => ['@redis.factory', '@cache_tags_provider.container', '@serialization.phpserialize'],
+        ],
+        'cache.container' => [
+          'class' => '\Drupal\redis\Cache\PhpRedis',
+          'factory' => ['@cache.backend.redis', 'get'],
+          'arguments' => ['container'],
+        ],
+        'cache_tags_provider.container' => [
+          'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
+          'arguments' => ['@redis.factory'],
+        ],
+        'serialization.phpserialize' => [
+          'class' => 'Drupal\Component\Serialization\PhpSerialize',
+        ],
+      ],
+    ];
+  }
 }
 
-if (isset($_ENV['PLATFORM_APP_DIR'])) {
+if (getenv('PLATFORM_APP_DIR')) {
 
   // Configure private and temporary file paths.
   if (!isset($settings['file_private_path'])) {
-    $settings['file_private_path'] = $_ENV['PLATFORM_APP_DIR'] . '/private';
+    $settings['file_private_path'] = getenv('PLATFORM_APP_DIR') . '/private';
   }
   if (!isset($config['system.file']['path']['temporary'])) {
-    $config['system.file']['path']['temporary'] = $_ENV['PLATFORM_APP_DIR'] . '/tmp';
+    $config['system.file']['path']['temporary'] = getenv('PLATFORM_APP_DIR') . '/tmp';
   }
 
   // Configure the default PhpStorage and Twig template cache directories.
@@ -54,15 +106,15 @@ if (isset($_ENV['PLATFORM_APP_DIR'])) {
 }
 
 // Set trusted hosts based on Platform.sh routes.
-if (isset($_ENV['PLATFORM_ROUTES'])) {
-  $routes = json_decode(base64_decode($_ENV['PLATFORM_ROUTES']), TRUE);
-  if (!isset($settings['trusted_host_patterns'])) {
-    $settings['trusted_host_patterns'] = [];
-  }
+if (getenv('PLATFORM_ROUTES') && !isset($settings['trusted_host_patterns'])) {
+  $routes = json_decode(base64_decode(getenv('PLATFORM_ROUTES')), TRUE);
+  $settings['trusted_host_patterns'] = [];
   foreach ($routes as $url => $route) {
     $host = parse_url($url, PHP_URL_HOST);
-    if ($host !== FALSE && $route['type'] == 'upstream' && $route['upstream'] == $_ENV['PLATFORM_APPLICATION_NAME']) {
-      $settings['trusted_host_patterns'][] = '^' . preg_quote($host) . '$';
+    if ($host !== FALSE && $route['type'] == 'upstream' && $route['upstream'] == getenv('PLATFORM_APPLICATION_NAME')) {
+      // Replace asterisk wildcards with a regular expression.
+      $host_pattern = str_replace('\*', '[^\.]+', preg_quote($host));
+      $settings['trusted_host_patterns'][] = '^' . $host_pattern . '$';
     }
   }
   $settings['trusted_host_patterns'] = array_unique($settings['trusted_host_patterns']);
@@ -70,8 +122,8 @@ if (isset($_ENV['PLATFORM_ROUTES'])) {
 
 // Import variables prefixed with 'd8settings:' into $settings and 'd8config:'
 // into $config.
-if (isset($_ENV['PLATFORM_VARIABLES'])) {
-  $variables = json_decode(base64_decode($_ENV['PLATFORM_VARIABLES']), TRUE);
+if (getenv('PLATFORM_VARIABLES')) {
+  $variables = json_decode(base64_decode(getenv('PLATFORM_VARIABLES')), TRUE);
   foreach ($variables as $name => $value) {
     // A variable named "d8settings:example-setting" will be saved in
     // $settings['example-setting'].
@@ -99,6 +151,11 @@ if (isset($_ENV['PLATFORM_VARIABLES'])) {
 
 // Set the project-specific entropy value, used for generating one-time
 // keys and such.
-if (isset($_ENV['PLATFORM_PROJECT_ENTROPY']) && empty($settings['hash_salt'])) {
-  $settings['hash_salt'] = $_ENV['PLATFORM_PROJECT_ENTROPY'];
+if (getenv('PLATFORM_PROJECT_ENTROPY') && empty($settings['hash_salt'])) {
+  $settings['hash_salt'] = getenv('PLATFORM_PROJECT_ENTROPY');
+}
+
+// Set the deployment identifier, which is used by some Drupal cache systems.
+if (getenv('PLATFORM_TREE_ID') && empty($settings['deployment_identifier'])) {
+  $settings['deployment_identifier'] = getenv('PLATFORM_TREE_ID');
 }
