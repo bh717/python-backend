d679e4cd446b9a91ce2f0d06968212a4c89a9cd7
WebDev717 <lasse.lin717@gmail.com>
2020-06-02 13:14:56 -0400

    CONT-0: Refactor Gitlab support

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -4,7 +4,7 @@ dir=$(dirname $0)
 
 set -ex
 
-cp ${dir}/settings.local.php ${dir}/../../web/sites/default/settings.local.php
+cp ${dir}/settings.local.php ${dir}/../web/sites/default/settings.local.php
 
 sed -ri -e "s!/var/www/html/web!$CI_PROJECT_DIR/web!g" /etc/apache2/sites-available/*.conf
 sed -ri -e "s!/var/www/html/web!$CI_PROJECT_DIR/web!g" /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
