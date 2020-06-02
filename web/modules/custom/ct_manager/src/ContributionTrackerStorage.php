5ca83f180c88a86cf155c95f15960cef7013baec
WebDev717 <lasse.lin717@gmail.com>
2020-06-02 15:38:03 +0000

    Add support for storing issues and comments

--- web/modules/custom/ct_manager/src/ContributionTrackerStorage.php
+++ web/modules/custom/ct_manager/src/ContributionTrackerStorage.php
@@ -40,41 +40,125 @@ class ContributionTrackerStorage {
   }
 
   /**
-   * Store issue and comments of user.
-   */
-  public function storeData($userContribtions) {
-    $issues = $userContribtions['issueComments'];
-    $issueComments = $userContribtions['issueComments'];
-    $issues ? $this->saveIssue($issues) : [];
-    $issueComments ? $this->saveIssueComments($issueComments) : [];
-  }
-
-  /**
    * Save Issues.
    */
-  public function saveIssue($issues) {
-    foreach ($issues as $value) {
-      $node = $this->nodeStorage->create($value);
-      $node_saved = $node->save();
-      if ($node_saved) {
-        $this->logger->notice('Node for Issue is Created with node id : @nid', ['@nid' => $node->id()]);
-      }
-    }
+  public function saveIssue($issue) {
+    $title = $issue['title'] ?: '(not found)';
+    // Create an issue.
+    $node = $this->nodeStorage->create([
+      'type' => 'issue',
+      'title' => $title,
+      'field_issue_link' => $issue['url'],
+    ]);
+    $node->save();
+    return $node;
 
   }
 
   /**
-   * Save IssueComments.
+   * Save code contributions.
    */
-  public function saveIssueComments($issueComments) {
-    foreach ($issueComments as $value) {
-      $node = $this->nodeStorage->create($value);
-      $node_saved = $node->save();
-      if ($node_saved) {
-        $this->logger->notice('Node For IssueComments is Created with node id : @nid', ['@nid' => $node->id()]);
+  public function saveCodeContribution($comment, $issueNode, $user) {
+    $commentBody = '';
+
+    if (empty($comment['title'])) {
+      $comment['title'] = 'Comment on ' . $issueNode->getTitle();
+      if (!empty($comment['description'])) {
+        $commentBody = $comment['description'];
+        $comment['title'] = strip_tags($commentBody);
+        if (strlen($comment['title']) > 80) {
+          $comment['title'] = substr($comment['title'], 0, 77) . '...';
+        }
       }
     }
 
+    $node = $this->nodeStorage->create([
+      'type' => 'code_contribution',
+      'title' => $comment['title'],
+      'field_code_contrib_link' => $comment['url'],
+      'field_contribution_author' => $user->id(),
+      'field_contribution_date' => date('Y-m-d', $comment['created']),
+      'field_contribution_description' => [
+        'value' => $commentBody,
+        'format' => 'basic_html',
+      ],
+      'field_code_contrib_issue_link' => $issueNode->id(),
+      'field_code_contrib_project' => $this->getOrCreateTerm($comment['project'], 'project')->id(),
+      'field_code_contrib_issue_status' => $comment['status'] ?: NULL,
+      'field_contribution_technology' => $comment['technology'] ?: NULL,
+      'field_code_contrib_files_count' => $comment['file'] ?: 0,
+      'field_code_contrib_patches_count' => $comment['patch'] ?: 0,
+    ]);
+    $node->save();
+    return $node;
+
+  }
+
+  /**
+   * Get issue of type issue.
+   */
+  public function getNodeForIssue($issueLink) {
+    $issues = $this->nodeStorage->getQuery()
+      ->condition('type', 'issue')
+      ->condition('field_issue_link', $issueLink)
+      ->execute();
+
+    return (count($issues) > 0) ? $this->nodeStorage->load(reset($issues)) : NULL;
+  }
+
+  /**
+   * Get node of type code_contribution.
+   */
+  public function getNodeForCodeContribution($commentLink) {
+    $nodes = $this->nodeStorage->getQuery()
+      ->condition('type', 'code_contribution')
+      ->condition('field_code_contrib_link', $commentLink)
+      ->execute();
+
+    return (count($nodes) > 0) ? $this->nodeStorage->load(reset($nodes)) : NULL;
+  }
+
+  /**
+   * Retrieves the node detail for issues.
+   */
+  public function getOrCreateIssueNode($issueUrl, $issueTitle) {
+    $issueNode = $this->getNodeForIssue($issueUrl);
+    if (!$issueNode) {
+      $issue = [];
+      $issue['title'] = $issueTitle;
+      $issue['url'] = $issueUrl;
+      $issueNode = $this->saveIssue($issue);
+    }
+    return $issueNode;
+  }
+
+  /**
+   * Get (or create) a term in a specified vocabulary.
+   *
+   * @param string $termName
+   *   Name of the term to be retrieved or created.
+   * @param string $vocabulary
+   *   Machine name of the vocabulary.
+   *
+   * @return \Drupal\taxonomy\TermInterface
+   *   The term with the given name in the given vocabulary.
+   */
+  protected function getOrCreateTerm($termName, $vocabulary) {
+    $terms = $this->termStorage->getQuery()
+      ->condition('name', $termName)
+      ->condition('vid', $vocabulary)
+      ->execute();
+
+    if (count($terms) == 0) {
+      $term = $this->termStorage->create([
+        'name' => $termName,
+        'vid' => $vocabulary,
+      ]);
+      $term->save();
+      return $term;
+    }
+
+    return $this->termStorage->load(reset($terms));
   }
 
 }
--- web/modules/custom/ct_manager/src/Plugin/QueueWorker/ProcessUsers.php
+++ web/modules/custom/ct_manager/src/Plugin/QueueWorker/ProcessUsers.php
@@ -65,9 +65,39 @@ class ProcessUsers extends QueueWorkerBase implements ContainerFactoryPluginInte
    * Collects user contribution and stores it.
    */
   public function processItem($data) {
-    $plugin_instance = $this->pluginManager->createInstance($data->plugin_id);
-    $userContribution = $plugin_instance->getUserInformation($data);
-    $this->contribStorage->storeData($userContribution);
+    $plugin_id = $data->plugin_id;
+    $user = $data->user;
+    $plugin_instance = $this->pluginManager->createInstance($plugin_id);
+    try {
+      $plugin_instance->getUserInformation($user);
+    }
+    catch (\RuntimeException $ex) {
+      // @TODO: Make exception classes more specific.
+      $this->logger->error('@plugin username for @username is invalid.', ['@plugin' => $plugin_id, '@username' => $user->getUsername()]);
+      return;
+    }
+
+    $issues = $plugin_instance->getUserIssues($user);
+    $comments = $plugin_instance->getUserComments($user);
+
+    $this->logger->notice('Saving @plugin issues for @user', ['@plugin' => $plugin_id, '@user' => $user->getUsername()]);
+    foreach ($issues as $issue) {
+      if ($this->contribStorage->getNodeForIssue($issue['url'])) {
+        $this->logger->notice('Skipping issue @issue, and all after it.', ['@issue' => $issue['url']]);
+        break;
+      }
+      $this->contribStorage->saveIssue($issue);
+    }
+
+    $this->logger->notice('Saving @plugin code contributions for @user', ['@plugin' => $plugin_id, '@user' => $user->getUsername()]);
+    foreach ($comments as $comment) {
+      if ($this->contribStorage->getNodeForCodeContribution($comment['url'])) {
+        $this->logger->notice('Skipping code contribution @comment, and all after it.', ['@comment' => $comment['url']]);
+        break;
+      }
+      $issueNode = $this->contribStorage->getOrCreateIssueNode($comment['issueUrl'], $comment['issueTitle']);
+      $this->contribStorage->saveCodeContribution($comment, $issueNode, $user);
+    }
   }
 
 }
