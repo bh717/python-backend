a52959f658dbde97da8edc9afba5e41d59f9ede9
WebDev717 <lasse.lin717@gmail.com>
2020-06-10 12:40:09 +0000

    CONT-107 Added remaining getter and setter function

--- web/modules/custom/ct_manager/src/Data/CodeContribution.php
+++ web/modules/custom/ct_manager/src/Data/CodeContribution.php
@@ -25,6 +25,36 @@ final class CodeContribution {
   protected string $description;
 
   /**
+   * @var \DateTimeImmutable Created date.
+   */
+  protected \DateTimeImmutable $created;
+
+  /**
+   * @var string Project.
+   */
+  protected string $project;
+
+  /**
+   * @var int Patch count.
+   */
+  protected int $patchCount;
+
+  /**
+  * @var int File count.
+  */
+  protected int $filesCount;
+
+  /**
+   * @var int Status.
+   */
+  protected int $status;
+
+  /**
+   * @var string Technology.
+   */
+  protected string $technology;
+
+  /**
    * @var \Drupal\ct_manager\Data\Issue Related issue.
    */
   protected Issue $issue;
@@ -53,6 +83,69 @@ final class CodeContribution {
 
   public function setDescription(string $description): self {
     $this->description = $description;
+    return $this;
+  }
+
+  public function getCreated(): \DateTimeImmutable {
+    return $this->created;
+  }
+
+  public function setCreated(string $created): self {
+    $this->created = $created;
+    return $this;
+  }
+
+  public function getProject(): string {
+    return $this->project;
+  }
+
+  public function setProject(string $project): self {
+    $this->project = $project;
+    return $this;
+  }
+
+  public function getIssue(): Issue {
+    return $this->issue;
+  }
+
+  public function setIssue(string $title, string $url): self {
+    $this->issue = new Issue($title, $url);
+    return $this;
+  }
+
+  public function getPatchCount(): int {
+    return $this->patchCount;
+  }
+
+  public function setPatchCount(int $patchCount): self {
+    $this->patchCount = $patchCount;
+    return $this;
+  }
+
+  public function getFilesCount(): int {
+    return $this->filesCount;
+  }
+
+  public function setFilesCount(int $filesCount): self {
+    $this->filesCount = $filesCount;
+    return $this;
+  }
+
+  public function getStatus(): int {
+    return $this->status;
+  }
+
+  public function setStatus(int $status): self {
+    $this->status = $status;
+    return $this;
+  }
+
+  public function getTechnology(): string {
+    return $this->technology;
+  }
+
+  public function setTechnology(string $technology): self {
+    $this->technology = $technology;
     return $this;
   }
 
