87f32c30b6cdb723934b6a2ae77a6af895c5d964
WebDev717 <lasse.lin717@gmail.com>
2021-05-20 17:17:29 +0000

    Fixed the issues raised in last MR

--- web/themes/custom/contribtracker/ts/navigation.ts
+++ web/themes/custom/contribtracker/ts/navigation.ts
@@ -1,24 +1,21 @@
 /////////////////////////////
 //      Selectors
 /////////////////////////////
-const navlists = document.querySelectorAll('.nav__menu-item-expanded');
 const sideBar = document.querySelector('.nav__sidebar-menu');
-const burger = document.querySelector('.burger');
-
-/////////////////////////////
-//      Nav Dropdown
-/////////////////////////////
-navlists?.forEach((list) => {
-  list?.querySelector('ul')?.classList.add('nav__dropdown-closed');
-});
+const burger = document.querySelector('.nav__burger');
+const branding = document.querySelector('.branding');
 
 /////////////////////////////
 //   Burger Click Listener
 /////////////////////////////
 burger?.addEventListener('click', () => {
-  if (sideBar?.classList.contains('show')) {
-    sideBar.classList.toggle('hide');
-  } else if (sideBar?.classList.contains('hide')) {
-    sideBar.classList.toggle('show');
-  }
+  branding?.querySelector('a')?.classList.add('nav__sidebar-menu_link');
+  document
+    ?.querySelector('.nav__item-account')
+    ?.classList.add('nav__sidebar_ul-account');
+  sideBar
+    ?.querySelector('.nav__auth-link')
+    ?.classList.add('nav__sidebar_auth-link');
+  sideBar?.querySelector('.nav__item')?.classList.add('nav__sidebar_item');
+  sideBar?.classList.toggle('is-visible');
 });
