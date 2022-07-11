ab2419cc773b92ebe4fc1f1224e02c4eaa99ee88
WebDev717 <lasse.lin717@gmail.com>
2022-07-11 11:12:45 -0400

    chore: remove deprecated plugins config after cypress migration

--- web/themes/custom/contribtracker/cypress.config.ts
+++ web/themes/custom/contribtracker/cypress.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     baseUrl: 'http://contribtracker.lndo.site/',
   },
-})
+});
