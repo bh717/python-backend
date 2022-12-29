e7ecd2cb87394b1140c01f3caa7b4d7de3991464
WebDev717 <108454005+WebDev717@users.noreply.github.com>
2022-12-29 13:08:52 -0500

    Update bootstrap-unit.php

--- bootstrap-unit.php
+++ bootstrap-unit.php
@@ -1,15 +1,1 @@
-<?php
 
-/**
- * @file
- * A fast bootstrap file for unit tests.
- */
-
-use weitzman\DrupalTestTraits\AddPsr4;
-
-[$finder, $class_loader] = AddPsr4::add();
-$root = $finder->getDrupalRoot();
-
-// Register more namespaces, as needed.
-$class_loader->addPsr4('Drupal\ct_manager\\', "$root/modules/custom/ct_manager/src");
-$class_loader->addPsr4('Drupal\Tests\ct_manager\\', "$root/modules/custom/ct_manager/tests/src");
